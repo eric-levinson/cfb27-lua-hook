b77229904aa4ccf5d83dda0e3ad934be0e12bea8
Eric <eric.levinson42@gmail.com>
2026-07-11 11:52:23 -0500

    Add Lua hook Node client

--- packages/sdk/index.cjs
+++ packages/sdk/index.cjs
@@ -1,13 +1,14 @@
 'use strict';
 
 const { ERROR_CODES, Cfb27HookError } = require('./src/errors.cjs');
-const { MAX_FRAME_BYTES, encodeFrame, FrameDecoder } = require('./src/frame.cjs');
+const { discoverGame } = require('./src/process.cjs');
+const { createClient, getHostStatus, runScriptFile } = require('./src/client.cjs');
 
-module.exports = Object.freeze({
-  version: '0.1.0-dev.1',
+module.exports = {
   ERROR_CODES,
   Cfb27HookError,
-  MAX_FRAME_BYTES,
-  encodeFrame,
-  FrameDecoder,
-});
+  discoverGame,
+  createClient,
+  getHostStatus,
+  runScriptFile,
+};
